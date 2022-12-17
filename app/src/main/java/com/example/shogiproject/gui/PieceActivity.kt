c4733273c5c0479ec3843f19f1ab5f34e48ac145
Whisky92 <olah.tomi.92@gmail.com>
2022-12-17 18:46:13 +0100

    Creating the main page of the application with buttons on it

---
+++ app/src/main/java/com/example/shogiproject/gui/PieceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shogiproject.gui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PieceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_piece)
+    }
+}
